0243c38e3d42558dca5f305fa4edf343ddc96700
Olga Kroshkina <olkroshk@microsoft.com>
2020-12-07 20:41:20 -0800

    added regex

--- maa-validate-jwt/src/main.cpp
+++ maa-validate-jwt/src/main.cpp
@@ -13,98 +13,7 @@
 #include <base64.hpp>
 #include <regex>
 
-// Parse JSON
-#include <unordered_set>
-
-enum class JsonType {
-    kJsonTypeUndef = 0,
-    kJsonTypeBool = 1,
-    kJsonTypeInt = 2,
-    kJsonTypeFloat = 3,
-    kJsonTypeString = 4,
-    kJsonTypeObject = 5
-};
-
-const std::unordered_set<char> kEscapeCharacters = {'\\'};
-const std::unordered_set<char> kSpecialCharacters = { '{', '}', '"', ',', ':', '[', ']'};
-
-struct Json {
-
-    std::vector<Json*> json_values;
-    std::vector<bool> bool_values;
-    std::vector<int> int_values;
-    std::vector<double> double_values;
-    std::vector<std::string> string_values;
-
-    JsonType my_type;
-
-    std::unordered_map<std::string, Json> object_data;
-
-    Json() {}
-
-    explicit Json(const std::string& doc): my_type(JsonType::kJsonTypeObject) {
-        const size_t sz = doc.size();
-        for (size_t i = 0; i < sz; ++i) {
-            switch (doc[i]) {
-            case '{':
-                my_type = JsonType::kJsonTypeObject;
-                size_t start = i + 1;
-                size_t end = doc.find_last_of('}');
-                if (start < sz && end != std::string::npos) {
-                    json_values.push_back(new Json());
-                    parse_object(doc.substr(start, end-start), object_data);
-                }
-                i = end;
-                break;
-            case '\\':
-
-
-            default:
-
-            }
-        }
-        
-    }
-
-    ~Json() {
-        if (!json_values.empty()) {
-            for (int i = 0; i < json_values.size(); ++i) {
-                delete json_values[i];
-                json_values[i] = nullptr;
-            }
-            json_values.clear();
-        }
-    }
-
-    // I don't think I need to write here a real JSONparser
-    void parse_object(const std::string& value, std::unordered_map<std::string, Json>& obj) {
-        const size_t sz = value.size();
-        for (size_t i = 0; i < sz - 1; ++i) {
-            size_t start = i + 1;
-            switch (value[i]) {
-            case '{':
-                my_type = JsonType::kJsonTypeObject;
-                size_t end = value.find_last_of('}');
-                if (end != std::string::npos) {
-                    json_values.push_back(new Json());
-                    parse_object(value.substr(start, end - start), object_data);
-                }
-                i = end;
-                break;
-            case '\\':
-
-                parse_string(start, );
-
-
-            default:
-
-            }
-            
-        }
-
-    }
-
-};
+// PArse JSON - for this sample no need to fully parse JSON, this sample is meant to show token validation, while using limited number of third-party libraries
 
 // utils
 inline void split_string(const std::string& str, const std::string& delim, std::vector<std::string>& result) {
@@ -112,15 +21,22 @@ inline void split_string(const std::string& str, const std::string& delim, std::
     result = std::vector<std::string>( std::sregex_token_iterator(str.begin(), str.end(), regex, -1), std::sregex_token_iterator());
 }
 
-struct Jwt {
+struct JwtMaster {
     std::string encoded_token;
+    
     std::string encoded_header;
     std::string encoded_payload;
     std::string encoded_signature;
+
     std::string decoded_header;
     std::string decoded_payload;
 
-    Jwt(const std::string& token): encoded_token(token) {
+    std::string jku; // TODO olga write me
+    std::string kid; // tODO olga write description
+    std::string attest_dns;
+    std::string tenant;
+
+    JwtMaster(const std::string& token): encoded_token(token) {
         std::vector<std::string> hps; // hps = header, payload, signature
         split_string(token, "\\.", hps);
         
@@ -130,8 +46,57 @@ struct Jwt {
         encoded_header = hps[0];
         encoded_payload = hps[1];
         encoded_signature = hps[2];
+
         decoded_header = base64::uri_decode(encoded_header);
         decoded_payload = base64::uri_decode(encoded_payload);
+        
+        jku = retrieve_jku();
+        kid = retrieve_kid();
+        attest_dns = retreive_dns();
+        tenant = retreive_tenant();
+    }
+
+private:
+    std::string retrieve_jku() const {
+        if (decoded_header.empty()) {
+            std::cout << "Empty decoded JWT header, cannot retrieve 'jku' token" << std::endl;
+            return "";
+        }
+        std::regex rgx(".*[jJ][kK][uU] *\" *: *\" *([0-9a-zA-Z\:\/\.]*)");
+        std::smatch match;
+        std::string result = (std::regex_search(decoded_header.begin(), decoded_header.end(), match, rgx)) ? std::string(match[1]) : "";
+        return result;
+    }
+
+    std::string retrieve_kid() const {
+        if (decoded_header.empty()) {
+            std::cout << "Empty decoded JWT header, cannot retrieve 'kid' token" << std::endl;
+            return "";
+        }
+        std::regex rgx(".*[kK][iI][dD] *\" *: *\" *([0-9a-zA-Z=]*)");
+        std::smatch match;
+        std::string result = (std::regex_search(decoded_header.begin(), decoded_header.end(), match, rgx)) ? std::string(match[1]) : "";
+        return result;
+    }
+
+    std::string retreive_dns() const {
+        if (decoded_header.empty()) {
+            std::cout << "Empty decoded JWT header, cannot retrieve attest DNS" << std::endl;
+            return "";
+        }
+        std::regex rgx("https:\/\/([0-9a-zA-Z\.]*)");
+        std::smatch match;
+        std::string result = (std::regex_search(decoded_header.begin(), decoded_header.end(), match, rgx)) ? std::string(match[1]) : "";
+        return result;
+    }
+
+    std::string retreive_tenant() const {
+        if (attest_dns.empty()) {
+            std::cout << "Empty attest DNS, cannot retrieve tenant name" << std::endl;
+            return "";
+        }
+        std::string result = attest_dns.substr(0, attest_dns.find_first_of("."));
+        return result;
     }
 };
 
@@ -160,7 +125,7 @@ int main(int argc, char* argv[], char* envp[]) {
     }
     infile.close();
 
-    Jwt jwt(maybe_encoded_tokens[0]);
+    JwtMaster jwt(maybe_encoded_tokens[0]);
 
     std::cout << jwt.decoded_header << std::endl;
     // Call certs

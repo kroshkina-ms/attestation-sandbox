6939ea732019a4bf8f6a782dfc9f07d181a5e7b7
Ubuntu <olkroshk@olkroshk-accvm.sl3znmtjq1fubjcw4kfjmuqv0c.fx.internal.cloudapp.net>
2022-03-29 00:31:00 +0000

    testing

---
+++ dev-variants/verifier-ubuntu-cmake-vcpkg/ubuntu-bootstrap-mvj.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# abort on nonzero exitstatus
+set -o errexit
+# abort on unbound variable
+set -o nounset   
+# do not hide errors within pipes
+set -o pipefail  
+
+function __msg() {
+    echo -e "    $*"
+}
+
+function __msg_stage() {
+    echo -e "********************************************************************"
+    echo -e "*   $*"
+    echo -e "********************************************************************"
+}
+
+__msg_stage "Install tools"
+
+sudo apt-get update
+#sudo apt install -y make cmake g++ libsgx-dcap-ql libsgx-dcap-ql-dev az-dcap-client open-enclave-hostverify
+sudo apt install -y make cmake g++
+
+__msg_stage "Set up development environment and install the dependencies"
+
+export LOCAL_ROOT=$(pwd)
+__msg "LOCAL_ROOT is set to $LOCAL_ROOT"
+
+export VCPKG_ROOT="../../vcpkg"
+__msg "VCPKG_ROOT is set to $VCPKG_ROOT"
+
+git submodule update --init --recursive
+cd ./$VCPKG_ROOT
+./bootstrap-vcpkg.sh
+./vcpkg integrate install
+./vcpkg install curl[openssl] zlib openssl --triplet x64-linux
+cd $LOCAL_ROOT
+
+__msg_stage "Clean and build MAA JWT validation tool"
+
+JWT_VALIDATOR_SRC_DIR=$LOCAL_ROOT"/dev-variants/verifier-ubuntu-cmake-vcpkg"
+JWT_VALIDATOR_OUT_DIR=$JWT_VALIDATOR_SRC_DIR"/out"
+
+cd $JWT_VALIDATOR_SRC_DIR
+rm --recursive --force --verbose $JWT_VALIDATOR_OUT_DIR
+mkdir --verbose --parents $JWT_VALIDATOR_OUT_DIR
+cd $JWT_VALIDATOR_OUT_DIR
+cmake $JWT_VALIDATOR_SRC_DIR
+make
+
+__msg "MAA JWT validation tool was built. Executable is located in $JWT_VALIDATOR_OUT_DIR"
+ls -l --all
+./jwt-verifier
+
